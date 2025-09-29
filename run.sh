53bd33119b791297dc4ca0b6738470d97992da20
YUNBLAK <87653966+YUNBLAK@users.noreply.github.com>
2025-09-28 18:35:19 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,98 @@
+python main.py --dataset c10 --seed 52 --model-name res20s --sam --zsam
+python main.py --dataset c10 --seed 52 --model-name res20s --samsung
+python main.py --dataset c10 --seed 52 --model-name res20s --sam
+python main.py --dataset c10 --seed 52 --model-name res20s --fsam
+python main.py --dataset c10 --seed 52 --model-name res20s
+
+python main.py --dataset c10 --seed 52 --model-name res56s --sam --zsam
+python main.py --dataset c10 --seed 52 --model-name res56s --samsung
+python main.py --dataset c10 --seed 52 --model-name res56s --sam
+python main.py --dataset c10 --seed 52 --model-name res56s --fsam
+python main.py --dataset c10 --seed 52 --model-name res56s
+
+python main.py --dataset c10 --seed 52 --model-name vgg16_bn --sam --zsam
+python main.py --dataset c10 --seed 52 --model-name vgg16_bn --samsung
+python main.py --dataset c10 --seed 52 --model-name vgg16_bn --sam
+python main.py --dataset c10 --seed 52 --model-name vgg16_bn --fsam
+python main.py --dataset c10 --seed 52 --model-name vgg16_bn
+
+python main.py --dataset c10 --seed 52 --model-name vgg19_bn --sam --zsam
+python main.py --dataset c10 --seed 52 --model-name vgg19_bn --samsung
+python main.py --dataset c10 --seed 52 --model-name vgg19_bn --sam
+python main.py --dataset c10 --seed 52 --model-name vgg19_bn --fsam
+python main.py --dataset c10 --seed 52 --model-name vgg19_bn
+
+python main.py --dataset c10 --seed 52 --model-name vit --sam --zsam
+python main.py --dataset c10 --seed 52 --model-name vit --samsung
+python main.py --dataset c10 --seed 52 --model-name vit --sam
+python main.py --dataset c10 --seed 52 --model-name vit --fsam
+python main.py --dataset c10 --seed 52 --model-name vit
+
+
+
+
+
+
+python main.py --dataset c100 --seed 52 --model-name res20s --sam --zsam
+python main.py --dataset c100 --seed 52 --model-name res20s --samsung
+python main.py --dataset c100 --seed 52 --model-name res20s --sam
+python main.py --dataset c100 --seed 52 --model-name res20s --fsam
+python main.py --dataset c100 --seed 52 --model-name res20s
+
+python main.py --dataset c100 --seed 52 --model-name res56s --sam --zsam
+python main.py --dataset c100 --seed 52 --model-name res56s --samsung
+python main.py --dataset c100 --seed 52 --model-name res56s --sam
+python main.py --dataset c100 --seed 52 --model-name res56s --fsam
+python main.py --dataset c100 --seed 52 --model-name res56s
+
+python main.py --dataset c100 --seed 52 --model-name vgg16_bn --sam --zsam
+python main.py --dataset c100 --seed 52 --model-name vgg16_bn --samsung
+python main.py --dataset c100 --seed 52 --model-name vgg16_bn --sam
+python main.py --dataset c100 --seed 52 --model-name vgg16_bn --fsam
+python main.py --dataset c100 --seed 52 --model-name vgg16_bn
+
+python main.py --dataset c100 --seed 52 --model-name vgg19_bn --sam --zsam
+python main.py --dataset c100 --seed 52 --model-name vgg19_bn --samsung
+python main.py --dataset c100 --seed 52 --model-name vgg19_bn --sam
+python main.py --dataset c100 --seed 52 --model-name vgg19_bn --fsam
+python main.py --dataset c100 --seed 52 --model-name vgg19_bn
+
+python main.py --dataset c100 --seed 52 --model-name vit --sam --zsam
+python main.py --dataset c100 --seed 52 --model-name vit --samsung
+python main.py --dataset c100 --seed 52 --model-name vit --sam
+python main.py --dataset c100 --seed 52 --model-name vit --fsam
+python main.py --dataset c100 --seed 52 --model-name vit
+
+
+
+
+
+python main.py --dataset svhn --seed 52 --model-name res20s --sam --zsam
+python main.py --dataset svhn --seed 52 --model-name res20s --samsung
+python main.py --dataset svhn --seed 52 --model-name res20s --sam
+python main.py --dataset svhn --seed 52 --model-name res20s --fsam
+python main.py --dataset svhn --seed 52 --model-name res20s
+
+python main.py --dataset svhn --seed 52 --model-name res56s --sam --zsam
+python main.py --dataset svhn --seed 52 --model-name res56s --samsung
+python main.py --dataset svhn --seed 52 --model-name res56s --sam
+python main.py --dataset svhn --seed 52 --model-name res56s --fsam
+python main.py --dataset svhn --seed 52 --model-name res56s
+
+python main.py --dataset svhn --seed 52 --model-name vgg16_bn --sam --zsam
+python main.py --dataset svhn --seed 52 --model-name vgg16_bn --samsung
+python main.py --dataset svhn --seed 52 --model-name vgg16_bn --sam
+python main.py --dataset svhn --seed 52 --model-name vgg16_bn --fsam
+python main.py --dataset svhn --seed 52 --model-name vgg16_bn
+
+python main.py --dataset svhn --seed 52 --model-name vgg19_bn --sam --zsam
+python main.py --dataset svhn --seed 52 --model-name vgg19_bn --samsung
+python main.py --dataset svhn --seed 52 --model-name vgg19_bn --sam
+python main.py --dataset svhn --seed 52 --model-name vgg19_bn --fsam
+python main.py --dataset svhn --seed 52 --model-name vgg19_bn
+
+python main.py --dataset svhn --seed 52 --model-name vit --sam --zsam
+python main.py --dataset svhn --seed 52 --model-name vit --samsung
+python main.py --dataset svhn --seed 52 --model-name vit --sam
+python main.py --dataset svhn --seed 52 --model-name vit --fsam
+python main.py --dataset svhn --seed 52 --model-name vit
